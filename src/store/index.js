5d09d5b9c71adccea348f4da71d4210287140f67
estherkim083 <estherkim083@gmail.com>
2023-01-05 16:41:06 +0900

    multiple-slices-redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { songsReducer, addSong, removeSong} from './slices/songsSlice'
-import { moviesReducer, addMovie, removeMovie } from './slices/moviesSlice'
-import { reset } from "./actions";
+import { carsReducer, addCar, deleteCar, changeSearchTerm } from './slices/carsSlice'
+import { formReducer, changeName, changeCost } from './slices/formSlice'
 
 const store= configureStore({
     reducer: {
-        songs: songsReducer,
-        movies: moviesReducer
+        cars: carsReducer,
+        form: formReducer
     }
-});
-
-
-export { store, reset, addSong, removeSong, addMovie, removeMovie };
-
-// const startingState= store.getState();
-// console.log(JSON.stringify(startingState));
-
-// store.dispatch(
-//     songsSlice.actions.addSong('Some song!!')
-// )
-
-// const finalState= store.getState();
-// console.log(JSON.stringify(finalState));
-// //console.log(songsSlice.actions.addSong('some song'))
+})
+export {store, changeName, changeCost, addCar, deleteCar, changeSearchTerm}
